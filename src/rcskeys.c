db99af6e701237ed220f1a300c9797c7cbee1ca1
Thien-Thi Nguyen <ttn@gnuvola.org>
2010-02-24 12:37:17 +0100

    Delete RCS keyword `Log' comments; nfc.

--- src/rcskeys.c
+++ src/rcskeys.c
@@ -20,41 +20,6 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-/*
- * $Log: rcskeys.c,v $
- * Revision 5.4  1995/06/16 06:19:24  eggert
- * Update FSF address.
- *
- * Revision 5.3  1993/11/03 17:42:27  eggert
- * Add Name keyword.
- *
- * Revision 5.2  1991/08/19  03:13:55  eggert
- * Say `T const' instead of `const T'; it's less confusing for pointer types.
- * (This change was made in other source files too.)
- *
- * Revision 5.1  1991/04/21  11:58:25  eggert
- * Don't put , just before } in initializer.
- *
- * Revision 5.0  1990/08/22  08:12:54  eggert
- * Add -k.  Ansify and Posixate.
- *
- * Revision 4.3  89/05/01  15:13:02  narten
- * changed copyright header to reflect current distribution rules
- * 
- * Revision 4.2  87/10/18  10:36:33  narten
- * Updating version numbers. Changes relative to 1.1 actuallyt
- * relative to 4.1
- * 
- * Revision 1.2  87/09/24  14:00:10  narten
- * Sources now pass through lint (if you ignore printf/sprintf/fprintf 
- * warnings)
- * 
- * Revision 4.1  83/05/04  10:06:53  wft
- * Initial revision.
- * 
- */
-
-
 #include "rcsbase.h"
 
 libId(keysId, "$Id: rcskeys.c,v 5.4 1995/06/16 06:19:24 eggert Exp $")
