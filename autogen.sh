92417700b855f9a559bf073dca92e76ceae9c5b9
Thien-Thi Nguyen <ttn@gnuvola.org>
2010-04-01 18:09:06 +0200

    [build] Add gnulib infrastructure (but don't use it yet).
    * m4/gnulib-cache.m4: New file, from "gnulib-tool --import".
    * autogen.sh: Use gnulib-tool.
    * configure.ac (gl_EARLY, gl_INIT): New.
    (AC_PROG_RANLIB): Delete.
    (AC_CONFIG_FILES): Add lib/Makefile.
    * Makefile.am (SUBDIRS): Add lib.
    (EXTRA_DIST): Add m4/gnulib-cache.m4.
    (ACLOCAL_AM_FLAGS): New var.

--- autogen.sh
+++ autogen.sh
@@ -5,12 +5,16 @@
 # Option "-f" means forcefully create symlinks for missing files
 # (by default: copies are made only if necessary).
 #
+# NB: You must have gnulib-tool on the PATH.
+#
 # Tested with:
+# - gnulib-tool (GNU gnulib 2010-03-28 11:28:34)
 # - autoconf (GNU Autoconf) 2.65
 # - automake (GNU automake) 1.11.1
 # - ltmain.sh (GNU libtool) 2.2.6b
 
 set -ex
+gnulib-tool --no-vc-files --import
 autoreconf --install --symlink "$@"
 
 # autogen.sh ends here
