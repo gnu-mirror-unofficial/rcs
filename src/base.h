2ab016181c3994425e99b0fddc92defca5e51c4f
Thien-Thi Nguyen <ttn@gnuvola.org>
2010-04-03 18:11:05 +0200

    [build] Move the config header (and template) from src/ to lib/.
    * configure.ac (AC_CONFIG_HEADERS): Rename to lib/config.h.
    
    * src/base.h: Don't #include "auto-sussed.h";
    instead, #include "config.h".
    * Makefile.am (AM_CPPFLAGS): Delete var.

--- src/base.h
+++ src/base.h
@@ -20,7 +20,7 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "auto-sussed.h"
+#include "config.h"
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
